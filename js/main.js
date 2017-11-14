398b4ba107d1882836bb1a7b1c2182e7d654f38f
Michelle Cinello <m.cinello@gmail.com>
2017-11-13 20:32:46 -0500

    can switch light to dark

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 
 
+var lightSwitch = document.querySelector('.plate');
+
+lightSwitch.addEventListener('click', function(e) {
+  var button = document.querySelector('button');
+  button.className = "switch off"
+  document.body.className = "dark";
+  var h1 = document.querySelector('h1');
+  h1.innerHTML = "Hey, who turned off the lights?"
+});
+
+
+// When someone hits the button in the switch, remove the current state class (on/off), and apply the opposite class (on/off)
+// Swap the colors of the body's background and text by adding or removing the dark/light class
+// Lastly, change the text in box to say "It's so bright in here!" when it's on, and "Hey, who turned off the lights?" when it's off
